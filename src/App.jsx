d25b6a4316cc5c9a3cb5cb4395cf1ba7c227f8cc
Ken Lewis <klewisjr@hotmail.com>
2024-07-06 16:23:51 -0700

    Basic appbar nav is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
 import './App.css'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import ClientProfile from "./pages/clients/ClientProfile.jsx";
-import AccountList from "./pages/accounts/AccountList.jsx";
-import AboutUs from "./pages/AboutUs.jsx";
-import StockList from "./pages/stocks/StockList.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
+import {BrowserRouter} from "react-router-dom";
 import Landing from "./pages/Landing.jsx";
 
 function App() {
   return (
-    <Landing>
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Dashboard/>}/>
-                <Route path='/profile' element={<ClientProfile/>}/>
-                <Route path='/accounts' element={<AccountList/>}/>
-                <Route path='/stocks' element={<StockList/>}/>
-                <Route path='/about-us' element={<AboutUs/>}/>
-            </Routes>
-        </BrowserRouter>
-    </Landing>
+      <BrowserRouter>
+          <Landing/>
+      </BrowserRouter>
   )
 }
 
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,194 @@
-import {Container} from "@mui/material";
+import {useState} from "react";
+import {AppBar, Avatar, Box, Button, Container, IconButton, Menu, Toolbar, Tooltip, Typography} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import {Link, Route, Routes} from 'react-router-dom';
+import Dashboard from "./Dashboard.jsx";
+import ClientProfile from "./clients/ClientProfile.jsx";
+import AccountList from "./accounts/AccountList.jsx";
+import StockList from "./stocks/StockList.jsx";
+import AboutUs from "./AboutUs.jsx";
 
 const Landing = function () {
-    return <Container>
-        <p>This is the Landing page.</p>
-    </Container>
+    const pages = ['Accounts', 'Stocks', 'About Us'];
+    const settings = ['Profile', 'Logout'];
+
+    const [anchorElNav, setAnchorElNav] = useState(null);
+    const [anchorElUser, setAnchorElUser] = useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    const Header = function () {
+        return (
+            <AppBar position="static">
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                        <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="a"
+                            href="#app-bar-with-responsive-menu"
+                            sx={{
+                                mr: 2,
+                                display: { xs: 'none', md: 'flex' },
+                                fontFamily: 'monospace',
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+                                color: 'inherit',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            LOGO
+                        </Typography>
+
+                        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleOpenNavMenu}
+                                color="inherit"
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'left',
+                                }}
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                sx={{
+                                    display: { xs: 'block', md: 'none' },
+                                }}
+                            >
+                                {pages.map((page) => (
+                                    <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                        <Typography textAlign="center">
+                                            <Link style={{textDecoration: "none", color: "white"}} to={`/${page.replace(/\s+/g, '-').toLowerCase()}`}>
+                                                {page}
+                                            </Link>
+                                        </Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
+                        <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                        <Typography
+                            variant="h5"
+                            noWrap
+                            component="a"
+                            href="#app-bar-with-responsive-menu"
+                            sx={{
+                                mr: 2,
+                                display: { xs: 'flex', md: 'none' },
+                                flexGrow: 1,
+                                fontFamily: 'monospace',
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+                                color: 'inherit',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            LOGO
+                        </Typography>
+                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                            {pages.map((page) => (
+                                <Button
+                                    key={page}
+                                    onClick={handleCloseNavMenu}
+                                    sx={{ my: 2, color: 'white', display: 'block' }}
+                                >
+                                    <Link style={{textDecoration: "none", color: "white"}} to={`/${page.replace(/\s+/g, '-').toLowerCase()}`}>
+                                        {page}
+                                    </Link>
+                                </Button>
+                            ))}
+                        </Box>
+
+                        <Box sx={{ flexGrow: 0 }}>
+                            <Tooltip title="Open settings">
+                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                    <Avatar alt="Ken Lewis" src="/static/images/avatar/2.jpg" />
+                                </IconButton>
+                            </Tooltip>
+                            <Menu
+                                sx={{ mt: '45px' }}
+                                id="menu-appbar"
+                                anchorEl={anchorElUser}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={Boolean(anchorElUser)}
+                                onClose={handleCloseUserMenu}
+                            >
+                                {settings.map((setting) => (
+                                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                        <Typography textAlign="center">
+                                            <Link style={{textDecoration: "none", color: "black"}} to={`/${setting.replace(/\s+/g, '-').toLowerCase()}`}>
+                                                {setting}
+                                            </Link>
+                                        </Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        );
+    }
+
+    const Footer = function () {
+        return (
+            <div>This is the footer</div>
+        )
+    }
+
+    return (
+        <Container maxWidth={'lg'}>
+            {Header()}
+            <div style={{height: "80vh"}}>
+                <Routes>
+                    <Route path='/' element={<Dashboard/>}/>
+                    <Route path='/profile' element={<ClientProfile/>}/>
+                    <Route path='/accounts' element={<AccountList/>}/>
+                    <Route path='/stocks' element={<StockList/>}/>
+                    <Route path='/about-us' element={<AboutUs/>}/>
+                </Routes>
+            </div>
+            {Footer()}
+        </Container>
+    )
 }
 
 export default Landing
